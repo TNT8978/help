3a5da78fefddeede14049d516ef6d775d6e44b0b
TNT8978 <41494106+TNT8978@users.noreply.github.com>
2022-01-28 22:25:23 -0800

    Update Towers.gd

--- Towers.gd
+++ Towers.gd
@@ -2,18 +2,33 @@ extends Node2D
 
 var enemy_array = []
 var built = false
+var enemy
 
 func _ready():
 	if built:
 		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[self.get_name()]["range"]
 
+# warning-ignore:unused_argument
 func _physics_process(delta):
-	trun()
+	if enemy_array.size() != 0 and built:
+		select_enemy()
+		trun()
+		#^^^ Error of " Invaild get index 'prsition' (on base: 'PathFollow2D (enemy1.gd)'). "
+	else:
+		enemy = null
 	
 func trun():
-	var enemy_prsition = get_global_mouse_position()
-	get_node("Turret").look_at(enemy_prsition)
+	get_node("Turret").look_at(enemy.prsition)
+	#^^^ Error of " Invaild get index 'prsition' (on base: 'PathFollow2D (enemy1.gd)'). "
 	
+func select_enemy(): 
+	var enemy_progress_array = []
+	for i in enemy_array:
+		enemy_progress_array.append(i.offset)
+	var max_offset = enemy_progress_array.max()
+	var enemy_index = enemy_progress_array.find(max_offset)
+	enemy = enemy_array[enemy_index]
+
 
 func _on_Range_body_entered(body):
 	enemy_array.append(body.get_parent())
