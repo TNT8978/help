ff4f99cccabc772709f791a95086069ade1350b5
TNT8978 <41494106+TNT8978@users.noreply.github.com>
2022-01-28 21:15:44 -0800

    Update GameData.gd

--- GameData.gd
+++ GameData.gd
@@ -1,13 +1,17 @@
 extends Node
 
 var tower_data = {
-#	Arrow Tower
+##		##
+## Arrow Tower  ##
+##		##
 	"ArrowTowerT1" : {
 		"damge" : 20,
 		"rof": 1,
-		"range": 100},
-#	Morter Tower
+		"range": 150},
+##		##
+## Morter Tower ##
+##		##
 	"MorterTowerT1": {
 		"damge": 50,
 		"rof": 5,
-		"range": 200}}
+		"range": 300}}
