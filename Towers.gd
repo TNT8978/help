cace431b57a761e4d895253d832f318df73bc4c5
TNT8978 <41494106+TNT8978@users.noreply.github.com>
2022-02-01 22:38:33 -0800

    Update Towers.gd

--- Towers.gd
+++ Towers.gd
@@ -1,25 +1,27 @@
 extends Node2D
 
+var type
 var enemy_array = []
 var built = false
 var enemy
+var ready = true
 
 func _ready():
 	if built:
-		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[self.get_name()]["range"]
+		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[type]["range"]
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
 	if enemy_array.size() != 0 and built:
 		select_enemy()
 		trun()
-		#^^^ Error of " Invaild get index 'prsition' (on base: 'PathFollow2D (enemy1.gd)'). "
+		if ready:
+			fire()
 	else:
 		enemy = null
 	
 func trun():
-	get_node("Turret").look_at(enemy.prsition)
-	#^^^ Error of " Invaild get index 'prsition' (on base: 'PathFollow2D (enemy1.gd)'). "
+	get_node("Turret").look_at(enemy.position)
 	
 func select_enemy(): 
 	var enemy_progress_array = []
@@ -28,7 +30,11 @@ func select_enemy():
 	var max_offset = enemy_progress_array.max()
 	var enemy_index = enemy_progress_array.find(max_offset)
 	enemy = enemy_array[enemy_index]
-
+func fire():
+	ready = false
+	enemy.on_hit(GameData.tower_data[type]["damge"])
+	yield(get_tree().create_timer(GameData.tower_data[type]["rof"]), "timeout")
+	ready = true
 
 func _on_Range_body_entered(body):
 	enemy_array.append(body.get_parent())
