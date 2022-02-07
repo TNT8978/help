18971c33e652bd1a776e22daf2f5a715d4209f05
TNT8978 <41494106+TNT8978@users.noreply.github.com>
2022-02-07 00:18:40 -0800

    Update GameData.gd

--- GameData.gd
+++ GameData.gd
@@ -1,47 +1,62 @@
 extends Node
 
 var tower_data = {
-##				##
+##		##
 ##Arrow  TowerT1##
-##				##
+##		##
 	"ArrowTowerT1": {
 		"damge" : 20,
-		"rof": 1,
-		"range": 150},
-##				##
+		"rof": 0.1,
+		"range": 250},
+##		##
 ##Morter TowerT1##
-##				##
+##		##
 	"MorterTowerT1": {
 		"damge": 50,
-		"rof": 2,
-		"range": 500},
-##				##
-## Arrow  Tower ##
-##				##
-	"ArrowTower" : {
-		"damge" : 20,
 		"rof": 1,
-		"range": 150},
-##				##
-## Morter Tower ##
-##				##
-	"MorterTower": {
-		"damge": 50,
-		"rof": 2,
-		"range": 500}}
+		"range": 500}
+}
 
-var wave_data_number = {
+var wave_data = {
 	"wave1": [
-		["Enemy1", 1.0], ["Enemy3", 1.0], ["Enemy1", 1.0], ["Enemy2", 1.0], ["Enemy1", 1.0]
+		["Enemy1", 1.0], ["Enemy1", 0.8], ["Enemy1", 0.6], ["Enemy1", 0.4], ["Enemy1", 0.2]
 	],
 	"wave2": [
-		["Enemy1", 1.0], ["Enemy1", 1.0], ["Enemy1", 1.0], ["Enemy1", 1.0], ["Enemy1", 1.0],
-		["Enemy1", 1.0], ["Enemy3", 1.0], ["Enemy3", 1.0], ["Enemy2", 1.0], ["Enemy2", 1.0]
+		["Enemy1", 1.0], ["Enemy1", 0.8], ["Enemy1", 0.6], ["Enemy1", 0.4], ["Enemy1", 0.2],
+		["Enemy1", 0.1], ["Enemy2", 0.5], ["Enemy2", 0.5], ["Enemy2", 0.5], ["Enemy2", 0.5]
 	],
 	"wave3":[
-		["Enemy1", 1.0], ["Enemy1", 1.0], ["Enemy1", 1.0], ["Enemy1", 1.0], ["Enemy1", 1.0],
-		["Enemy1", 1.0], ["Enemy1", 1.0], ["Enemy1", 1.0], ["Enemy1", 1.0], ["Enemy1", 1.0],
-		["Enemy1", 1.0], ["Enemy1", 1.0], ["Enemy3", 1.0], ["Enemy3", 1.0], ["Enemy3", 1.0],
-		["Enemy3", 1.0], ["Enemy2", 1.0], ["Enemy2", 1.0], ["Enemy2", 1.0], ["Enemy2", 1.0]
+		["Enemy1", 1.0], ["Enemy1", 0.9], ["Enemy1", 0.8], ["Enemy1", 0.7], ["Enemy1", 0.6],
+		["Enemy1", 0.5], ["Enemy1", 0.4], ["Enemy1", 0.3], ["Enemy1", 0.2], ["Enemy1", 0.1],
+		["Enemy1", 0.1], ["Enemy1", 0.1], ["Enemy3", 0.5], ["Enemy3", 0.5], ["Enemy3", 0.5],
+		["Enemy3", 0.5], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3]
+	],
+	"wave4":[
+		["Enemy1", 1.0], ["Enemy1", 0.9], ["Enemy1", 0.9], ["Enemy1", 0.8], ["Enemy1", 0.8], 
+		["Enemy1", 0.7], ["Enemy1", 0.7], ["Enemy1", 0.6], ["Enemy1", 0.6], ["Enemy1", 0.5], 
+		["Enemy1", 0.5], ["Enemy1", 0.4], ["Enemy1", 0.4], ["Enemy1", 0.3], ["Enemy1", 0.3], 
+		["Enemy3", 0.5], ["Enemy3", 0.5], ["Enemy3", 0.5], ["Enemy3", 0.5], ["Enemy3", 0.5], 
+		["Enemy3", 0.5], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3],
+		["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3]
+	],
+	"wave5":[
+		["Enemy1", 1.0], ["Enemy1", 0.9], ["Enemy1", 0.9], ["Enemy1", 0.9], ["Enemy1", 0.8], 
+		["Enemy1", 0.8], ["Enemy1", 0.8], ["Enemy1", 0.7], ["Enemy1", 0.7], ["Enemy1", 0.7], 
+		["Enemy1", 0.6], ["Enemy1", 0.6], ["Enemy1", 0.6], ["Enemy1", 0.5], ["Enemy1", 0.5], 
+		["Enemy1", 0.5], ["Enemy1", 0.4], ["Enemy1", 0.4], ["Enemy1", 0.4], ["Enemy1", 0.3], 
+		["Enemy1", 0.3], ["Enemy1", 0.3], ["Enemy1", 0.2], ["Enemy1", 0.2], ["Enemy1", 0.2], 
+		["Enemy3", 0.5], ["Enemy3", 0.5], ["Enemy3", 0.5], ["Enemy3", 0.5], ["Enemy3", 0.5], 
+		["Enemy3", 0.5], ["Enemy3", 0.5], ["Enemy3", 0.5], ["Enemy3", 0.5], ["Enemy3", 0.5], 
+		["Enemy3", 0.5], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3],
+		["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3],
+		["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3]
+	],
+	"wave6":[
+		["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3],
+		["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3], ["Enemy2", 0.3],
+		["EnemyBoss1", 1.0]
+	],
+	"wave7":[
+		["DeathReaper", 0.0]
 	]
 }
